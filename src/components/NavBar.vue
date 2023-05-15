f0ca82b0fc9c175323761cb130590a2d8e45e7ce
fgh319 <fengguihang@outlook.com>
2023-05-15 18:21:18 +0800

    创建Icon组件â

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="nav">
     <router-link to="/money">
-      <svg>
-        <use xlink:href="#money"></use>
-      </svg>
+      <Icon name="money"></Icon>
       记账
-    </router-link> |
-    <router-link to="/labels">标签</router-link> |
-    <router-link to="/statistics">统计</router-link>
+    </router-link>
+
+    <router-link to="/labels">
+      <Icon name="label"></Icon>
+      标签</router-link
+    >
+
+    <router-link to="/statistics">
+      <Icon name="statistics"></Icon>
+      统计</router-link
+    >
   </div>
 </template>
 
 <script lang="ts">
-  import money from '@/assets/icons/money.svg';
-  console.log(money);
-  export default {
-
-  };
+export default {};
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    background-color: lightgray;
-  }
+.nav {
+  background-color: lightgray;
+}
 </style>
