01ce43231ca7781d5404808f79161645f8d5a029
fgh319 <fengguihang@outlook.com>
2023-05-15 22:53:54 +0800

    添加NavBar样式

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money"></Icon>
       记账
     </router-link>
 
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="label"></Icon>
-      标签</router-link
-    >
+      标签
+    </router-link>
 
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"></Icon>
-      统计</router-link
-    >
-  </div>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -22,7 +22,21 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  background-color: lightgray;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+  padding: 5px 0;
+  font-size: 14px;
+  > .item {
+    width: 33.3333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  > .icon {
+    width: 28px;
+    height: 28px;
+  }
+  }
 }
 </style>
