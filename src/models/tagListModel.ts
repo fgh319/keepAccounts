71ac723e5499fb9511c28d57880f3f238013e45c
fgh319 <fengguihang@outlook.com>
2023-05-22 22:58:15 +0800

    完成EditLabel组件功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,16 +1,25 @@
-const localStorageKeyName = 'tagList';
+const localStorageKeyName = "tagList";
 
-export const tagListModel:{tags: Tag[], fetch():Tag[],pushTag(data:Tag):void,save(data:Tag[]):void} = {
+export const tagListModel: {
+  tags: Tag[];
+  fetch(): Tag[];
+  pushTag(data: Tag): void;
+  save(data: Tag[]): void;
+  remove(data:Tag):void;
+} = {
   tags: [],
   fetch() {
-    this.tags = JSON.parse(localStorage.getItem(localStorageKeyName) || '[]') ;
+    this.tags = JSON.parse(localStorage.getItem(localStorageKeyName) || "[]");
     return this.tags;
   },
   pushTag(data) {
     this.tags.push(data);
-    localStorage.setItem(localStorageKeyName, JSON.stringify(this.tags) );
+    localStorage.setItem(localStorageKeyName, JSON.stringify(this.tags));
   },
   save(data) {
     localStorage.setItem(localStorageKeyName, JSON.stringify(data));
-  }
-}
+  },
+  remove(data) {
+    this.tags.splice(this.tags.indexOf(data),1);
+  },
+};
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,84 +1,114 @@
 <script lang="ts">
-  import Vue from "vue";
-  import {Component} from "vue-property-decorator";
-  import {tagListModel} from "@/models/tagListModel";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { tagListModel } from "@/models/tagListModel";
 
-  @Component
-  export default class EditLabel extends Vue {
-    tag?:Tag = undefined;
-    created() {
-      const id = this.$route.params.id;
-      const tags = tagListModel.fetch();
-      const tag = tags.filter(t => t.id === id)[0];
-      if (tag) {
-        this.tag = tag;
-      } else {
-        this.$router.replace('/404')
-      }
+@Component
+export default class EditLabel extends Vue {
+  tags = tagListModel.fetch();
+  tag:Tag = {
+    id: '0',
+    value: '0',
+  };
+
+  created() {
+    const id = this.$route.params.id;
+    const tag = this.tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      this.tag = tag;
+    } else {
+      this.$router.replace("/404");
     }
   }
+
+  onValueChange() {
+    console.log(this.tags);
+    tagListModel.save(this.tags);
+  }
+
+  remove() {
+    if (window.confirm("确定删除吗？")) {
+      tagListModel.remove(this.tag);
+      tagListModel.save(this.tags);
+      this.$router.replace('/labels')
+    }
+  }
+  back() {
+    this.$router.back()
+  }
+}
 </script>
 
 <template>
   <layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"></Icon>
+      <Icon class="leftIcon" name="left" @click="back"></Icon>
       <span class="title">编辑标签</span>
     </div>
     <div class="name">
       <label>
         <span>标签名</span>
-        <input type="text" placeholder="请输入标签名" :value="tag.value" >
+        <input
+          @change="onValueChange"
+          v-model="tag.value"
+          type="text"
+          placeholder="请输入标签名"
+        />
       </label>
     </div>
     <div class="delTag-wrapper">
-      <button class="delTag">删除标签</button>
+      <button class="delTag" @click="remove">删除标签</button>
     </div>
   </layout>
 </template>
 
 <style scoped lang="scss">
-  .navBar {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: white;
-    padding: 12px ;
-    .leftIcon {
-      position: absolute;
-       left: 8px;
-    }
-    svg {
-      height: 1.3em;
-      width: 1.3em;
-      color: #666;
-    }
+.navBar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  padding: 12px;
+
+  .leftIcon {
+    position: absolute;
+    left: 8px;
   }
-  .name {
-    padding: 12px;
-    font-size: 14px;
-    background: white;
-    margin-top: 6px;
-    span {
-      margin-right: 12px;
-    }
-    input {
-      border: none;
-      background: transparent;
-    }
+
+  svg {
+    height: 1.3em;
+    width: 1.3em;
+    color: #666;
   }
-  .delTag {
+}
+
+.name {
+  padding: 12px;
+  font-size: 14px;
+  background: white;
+  margin-top: 6px;
+
+  span {
+    margin-right: 12px;
+  }
+
+  input {
     border: none;
-    background: #666;
-    color: white;
-    padding: 9px 18px;
-    border-radius: 4px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 24px;
-    }
+    background: transparent;
   }
+}
 
+.delTag {
+  border: none;
+  background: #666;
+  color: white;
+  padding: 9px 18px;
+  border-radius: 4px;
 
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 24px;
+  }
+}
 </style>
