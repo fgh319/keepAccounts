4d56ac7bc8ac84864a238dc773e8389ec1cc1840
fgh319 <fengguihang@outlook.com>
2023-05-24 11:10:55 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,11 +1,13 @@
 const localStorageKeyName = 'recordList';
 export const recordListModel = {
+  data:[] as RecordItem[],
   fetch() {
-    return JSON.parse(localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    this.data = JSON.parse(localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
   },
 
-  save(data: RecordItem[]) {
-    localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
   clone(data:RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
